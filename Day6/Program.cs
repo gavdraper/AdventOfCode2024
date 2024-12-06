f8c8c2a753ce0fffd714a87bb2983bb73c9b26b7
Gavin Draper <gavdraper@users.noreply.github.com>
2024-12-06 14:22:59 +0000

    Day 6 Part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,15 +3,15 @@ using System.Runtime.CompilerServices;
 
 var input = File.ReadAllText("input.txt");
 var lines = input.Split('\n').Select(x => x.ToCharArray()).ToList();
-
+var walkedPositions = new List<Point>();
 var positions = new Dictionary<string, int>();
 
 var directions = new List<Direction>()
 {
-    new Direction() { Character = '^', NextCharacter = '>', Velocity = new Point(0, -1) },
-    new Direction() { Character = '>', NextCharacter = 'v', Velocity = new Point(1, 0) },
-    new Direction() { Character = 'v', NextCharacter = '<', Velocity = new Point(0, 1) },
-    new Direction() { Character = '<', NextCharacter = '^', Velocity = new Point(-1, 0) },
+    new() { Character = '^', NextCharacter = '>', Velocity = new Point(0, -1) },
+    new() { Character = '>', NextCharacter = 'v', Velocity = new Point(1, 0) },
+    new() { Character = 'v', NextCharacter = '<', Velocity = new Point(0, 1) },
+    new() { Character = '<', NextCharacter = '^', Velocity = new Point(-1, 0) },
 };
 
 var currentLocation = FindGuard();
@@ -25,12 +25,50 @@ while (InBounds(currentLocation))
     currentLocation = nextLocation.Location;
     if (!InBounds(currentLocation))
         break;
+    if(!walkedPositions.Any(x=>x.X == currentLocation.X && x.Y == currentLocation.Y))
+    walkedPositions.Add(currentLocation);
     lines[currentLocation.Y][currentLocation.X] = nextLocation.Facing;
 }
 
-Console.WriteLine(positions.Count());
-Console.ReadLine();
+//Part 2
+var timeWarpOptions = 0;
+for (var i = 0; i < walkedPositions.Count; i++)
+{
+    var partTwoWalkedPositions = new List<LocationDirection>();
+    lines = input.Split('\n').Select(x => x.ToCharArray()).ToList();
+    currentLocation = FindGuard();
+    if (currentLocation.X == walkedPositions[i].X && currentLocation.Y == walkedPositions[i].Y)
+        continue;
+    lines[walkedPositions[i].Y][walkedPositions[i].X] = '#';
+    while (InBounds(currentLocation))
+    {
+        var nextLocation = GetNextLocation(currentLocation);
+        lines[currentLocation.Y][currentLocation.X] = '.';
+        currentLocation = nextLocation.Location;
+        if (!InBounds(currentLocation))
+            break;
+        if (partTwoWalkedPositions.Any(x => x.Location.X == currentLocation.X && x.Location.Y == currentLocation.Y && x.Facing == nextLocation.Facing))
+        {
+            timeWarpOptions++;
+            break;
+        }
+        partTwoWalkedPositions.Add(nextLocation);
+        lines[currentLocation.Y][currentLocation.X] = nextLocation.Facing;
+        
+        // Console.Clear();
+        // foreach (var l in lines)
+        // {
+        //     foreach (var c in l)
+        //         Console.Write(c);
+        //     Console.Write("\n");
+        // }
+      //  Console.ReadLine();
+    }
+}
 
+Console.WriteLine("Part 1 " + positions.Count());
+Console.WriteLine("Part 2 " + timeWarpOptions);
+Console.ReadLine();
 
 LocationDirection GetNextLocation(Point currentLocation)
 {
@@ -46,8 +84,6 @@ LocationDirection GetNextLocation(Point currentLocation)
     }
 }
 
-
-
 Point FindGuard()
 {
     for (var i = 0; i < lines.Count(); i++)
@@ -61,7 +97,6 @@ Point FindGuard()
             }
         }
     }
-
     return new Point(-1, -1);
 }
 
