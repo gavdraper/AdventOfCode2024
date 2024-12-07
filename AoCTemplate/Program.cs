1c322f1a90513bf5c016d3afa031d7bcdd6d5584
Gavin Draper <gavdraper@users.noreply.github.com>
2024-12-07 06:47:45 +0000

    Created AoC Day Template

---
+++ AoCTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using AocBase;
+
+var day = new DayX("","","","");
+day.Solve(true);
+
+public class DayX : Day
+{
+    public DayX(string expectedSampleResult, string expectedSampleResultTwo, string expectedResult,
+        string expectedResultTwo) : base(expectedSampleResult, expectedSampleResultTwo, expectedResult,
+        expectedResultTwo)
+    {
+    }
+
+    protected override string ProcessPartOne()
+    {
+        return "Hello";
+    }
+
+    protected override string ProcessPartTwo()
+    {
+        return "World";
+    }
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,23 @@
+﻿using AocBase;
+
+var day = new Day8("","","","");
+day.Solve(true);
+
+public class Day8 : Day
+{
+    public Day8(string expectedSampleResult, string expectedSampleResultTwo, string expectedResult,
+        string expectedResultTwo) : base(expectedSampleResult, expectedSampleResultTwo, expectedResult,
+        expectedResultTwo)
+    {
+    }
+
+    protected override string ProcessPartOne()
+    {
+        return "Hello";
+    }
+
+    protected override string ProcessPartTwo()
+    {
+        return "World";
+    }
+}
